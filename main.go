f3494f3a2385093b7a0ef0f47efaad94062637bb
rahulbalajee <rahrkb4@gmail.com>
2025-08-04 17:29:22 +0530

    solve chi router

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,13 +32,29 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
+func newPageHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), 30*time.Second)
+	defer cancel()
+
+	page := chi.URLParamFromCtx(ctx, "id")
+
+	fmt.Fprintf(w, "The page ID is %s", page)
+}
+
 func main() {
 	r := chi.NewRouter()
+	//r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Route("/galleries", func(r chi.Router) {
+		r.Use(middleware.Logger)
+		r.Get("/{id}", newPageHandler)
+	})
+	//r.Get("/page/{page-id}", newPageHandler)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
+		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
 	fmt.Println("Starting server on :3000...")
