984673ec22e69ea66b826fb809df1782fe9674ee
rahulbalajee <rahrkb4@gmail.com>
2025-08-20 22:46:52 +0530

    httponly cookie

--- controllers/users.go
+++ controllers/users.go
@@ -63,9 +63,10 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 	}
 
 	cookie := http.Cookie{
-		Name:  "email",
-		Value: user.Email,
-		Path:  "/",
+		Name:     "email",
+		Value:    user.Email,
+		Path:     "/",
+		HttpOnly: true,
 	}
 	http.SetCookie(w, &cookie)
 
