f18fcbb18aaf28224067f97f2d8cb59d6a150d70
rahulbalajee <rahrkb4@gmail.com>
2025-08-24 23:11:04 +0530

    stubbing out sessions

---
+++ models/session.go
@@ -0,0 +1,29 @@
+package models
+
+import "database/sql"
+
+type Session struct {
+	ID     int
+	UserID int
+	// Token is only set when creating a new session, otherwise empty
+	Token     string
+	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create session token here
+
+	// TODO: Implement SessionService.Create
+
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+
+	return nil, nil
+}
