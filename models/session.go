f185b4ca663fe1c445727ce3cc678dbcbfcd1159
rahulbalajee <rahrkb4@gmail.com>
2025-08-26 17:46:50 +0530

    finish up our sessions package

--- models/session.go
+++ models/session.go
@@ -72,9 +72,31 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	// TODO: Implement SessionService.User
+	// Hash the session token
+	tokenHash := ss.hash(token)
 
-	return nil, nil
+	// Query for that session wth the hash
+	var user User
+
+	row := ss.DB.QueryRow(`SELECT user_id FROM sessions WHERE token_hash = $1;`, tokenHash)
+
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+
+	// Using the UserID from the session query for that user
+	row = ss.DB.QueryRow(`
+		SELECT email, password
+		FROM users where id = $1`, user.ID)
+
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+
+	// Return the user
+	return &user, nil
 }
 
 func (ss *SessionService) hash(token string) string {
