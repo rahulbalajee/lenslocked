86bc3d60297af14125dae70224b83dc340061487
rahulbalajee <rahrkb4@gmail.com>
2025-08-26 18:08:26 +0530

    hook up handlers for signout

--- controllers/users.go
+++ controllers/users.go
@@ -11,6 +11,7 @@ import (
 type SessionService interface {
 	Create(userID int) (*models.Session, error)
 	User(token string) (*models.User, error)
+	Delete(token string) error
 }
 
 type Users struct {
@@ -107,4 +108,24 @@ func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Current user: %s\n", user.Email)
+}
+
+func (u Users) ProcessSignOut(w http.ResponseWriter, r *http.Request) {
+	token, err := readCookie(r, CookieSession)
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/signin", http.StatusFound)
+		return
+	}
+
+	err = u.SessionService.Delete(token)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	deleteCookie(w, CookieSession)
+
+	http.Redirect(w, r, "/signin", http.StatusFound)
 }
