990c0d689904ac26138aa3405968dc57a36fb434
rahulbalajee <rahrkb4@gmail.com>
2025-08-26 17:29:56 +0530

    update sessions instead of create each time

--- models/session.go
+++ models/session.go
@@ -44,12 +44,26 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		TokenHash: ss.hash(token),
 	}
 
+	// 1) Query for a user's session
+	// If found update it
+	// If not found create a new session
+
+	// Or skip (1) and try update if error create session
 	row := ss.DB.QueryRow(`
-		INSERT INTO sessions (user_id, token_hash)
-		VALUES ($1, $2)
+		UPDATE sessions
+		SET token_hash = $2
+		WHERE user_id = $1
 		RETURNING id;`, session.UserID, session.TokenHash)
 
 	err = row.Scan(&session.ID)
+
+	if err == sql.ErrNoRows {
+		row := ss.DB.QueryRow(`
+			INSERT INTO sessions (user_id, token_hash)
+			VALUES ($1, $2)
+			RETURNING id;`, session.UserID, session.TokenHash)
+		err = row.Scan(&session.ID)
+	}
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
