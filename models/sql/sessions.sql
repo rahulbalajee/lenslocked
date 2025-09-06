378145a715a0ad60f05f3d493bccb82c4cb1307f
rahulbalajee <rahrkb4@gmail.com>
2025-09-06 23:42:40 +0530

    clean up sql files

--- models/sql/sessions.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE, -- defines the relationship between the session and the user
-    token_hash TEXT UNIQUE NOT NULL
-);
--- models/sql/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
-);
