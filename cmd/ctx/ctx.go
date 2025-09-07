10c254ec1688b53610fe61d0f14ec1b90ca34743
rahulbalajee <rahrkb4@gmail.com>
2025-09-07 17:23:22 +0530

    custom type to store context keys

--- cmd/ctx/ctx.go
+++ cmd/ctx/ctx.go
@@ -5,8 +5,14 @@ import (
 	"fmt"
 )
 
+type ctxKey string
+
+const (
+	favoriteColorKey ctxKey = "favorite-color"
+)
+
 func main() {
-	ctx := context.WithValue(context.Background(), "favorite-color", "blue")
-	value := ctx.Value("favorite-color")
+	ctx := context.WithValue(context.Background(), favoriteColorKey, "blue")
+	value := ctx.Value(favoriteColorKey)
 	fmt.Println(value)
 }
