8c95ab5f9048f750c5f5c6375f2a6d5359c39c98
rahulbalajee <rahrkb4@gmail.com>
2025-09-07 17:56:56 +0530

    type assertion when pulling out of ctx

--- cmd/ctx/ctx.go
+++ cmd/ctx/ctx.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 )
 
 type ctxKey string
@@ -14,5 +15,12 @@ const (
 func main() {
 	ctx := context.WithValue(context.Background(), favoriteColorKey, "blue")
 	value := ctx.Value(favoriteColorKey)
-	fmt.Println(value)
+	strValue, ok := value.(string)
+	if !ok {
+		fmt.Println("not a string")
+		return
+	}
+
+	fmt.Println(strValue)
+	fmt.Println(strings.HasPrefix(strValue, "b"))
 }
