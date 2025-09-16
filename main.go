12197805b46ad251a0383acc2c4e2618d5ef0cf7
rahulbalajee <rahrkb4@gmail.com>
2025-09-16 23:44:25 +0530

    better comments

--- main.go
+++ main.go
@@ -31,11 +31,13 @@ func main() {
 	}
 
 	// Dependency injection (passing in the PostgreSQL DB)
+	//userService for creating and managing users
 	userService := models.UserService{
 		DB: db,
 	}
 
 	// Dependency injection (passing in the PostgreSQL DB)
+	//sessionService for creating and managing session
 	sessionService := models.SessionService{
 		DB: db,
 	}
