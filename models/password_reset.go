006b045439296ececbc34df689d58acebfb883d5
rahulbalajee <rahrkb4@gmail.com>
2025-09-21 17:03:48 +0530

    plumbing

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/rahulbalajee/lenslocked/rand"
 )
 
 const (
@@ -32,7 +37,58 @@ type PasswordResetService struct {
 }
 
 func (pr *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: Implement password reset service.Create")
+	email = strings.ToLower(email)
+
+	var userID int
+
+	row := pr.DB.QueryRow(`SELECT id FROM users WHERE email = $1`, email)
+	err := row.Scan(&userID)
+	if err != nil {
+		// TODO: consider returning a different error when user doesn't exists
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	bytesPerToken := pr.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	duration := pr.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+
+	pwReset := PasswordReset{
+		UserID:    userID,
+		Token:     token,
+		TokenHash: pr.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	// Insert pwReset into DB
+	row = pr.DB.QueryRow(`
+		INSERT INTO password_resets (user_id, token_hash, expires_at)
+		VALUES ($1, $2, $3) ON CONFLICT (user_id) DO
+		UPDATE
+		SET token_hash = $2, expires_at = $3
+		RETURNING id`, pwReset.UserID, pwReset.TokenHash, pwReset.ExpiresAt)
+
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	return &pwReset, nil
+}
+
+func (pr *PasswordResetService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
 
 func (pr *PasswordResetService) Consume(token string) (*User, error) {
