c11ceb5ebd93b68b3768d1dd5078c8c4be53a8a6
rahulbalajee <rahrkb4@gmail.com>
2025-09-21 16:28:09 +0530

    update controllers & models

--- controllers/users.go
+++ controllers/users.go
@@ -237,7 +237,12 @@ func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: Update the user's password
+	err = u.UserService.UpdatePassword(user.ID, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
 
 	session, err := u.SessionService.Create(user.ID)
 	if err != nil {
