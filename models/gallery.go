b207f22d5d79442d2f7fc9fd6b86c05a92d2bb19
rahulbalajee <rahrkb4@gmail.com>
2025-09-29 16:33:05 +0530

    finish up gallery service

--- models/gallery.go
+++ models/gallery.go
@@ -99,3 +99,14 @@ func (gs *GalleryService) Update(gallery *Gallery) error {
 
 	return nil
 }
+
+func (gs *GalleryService) Delete(id int) error {
+	_, err := gs.DB.Exec(`
+		DELETE FROM galleries 
+		WHERE id = $1`, id)
+	if err != nil {
+		return fmt.Errorf("delete gallery by id: %w", err)
+	}
+
+	return nil
+}
