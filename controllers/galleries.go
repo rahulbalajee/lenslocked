e0a69af3cf072fb8d187936f1e985735a38adb45
rahulbalajee <rahrkb4@gmail.com>
2025-09-29 23:18:47 +0530

    more handlers

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,15 +1,20 @@
 package controllers
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/rahulbalajee/lenslocked/context/context"
+	"github.com/rahulbalajee/lenslocked/models"
 )
 
 type Galleries struct {
 	Template struct {
-		New Executer
+		New  Executer
+		Edit Executer
 	}
 	GalleryService GalleryService
 }
@@ -38,4 +43,37 @@ func (g Galleries) ProcessNew(w http.ResponseWriter, r *http.Request) {
 
 	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
 	http.Redirect(w, r, editPath, http.StatusFound)
+}
+
+func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusNotFound)
+		return
+	}
+
+	gallery, err := g.GalleryService.ByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			http.Error(w, "Gallery not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "You are not authorized to edit this Gallery", http.StatusForbidden)
+		return
+	}
+
+	var data struct {
+		ID    int
+		Title string
+	}
+	data.ID = gallery.ID
+	data.Title = gallery.Title
+
+	g.Template.Edit.Execute(w, r, data)
 }
