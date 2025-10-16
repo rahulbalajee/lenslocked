ed0918ad0c223336e81cba5be3cf36bd32dfd5cc
rahulbalajee <rahrkb4@gmail.com>
2025-10-16 23:10:59 +0530

    add dropbox integration

---
+++ cmd/exp/exp.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dropboxID := os.Getenv("DROPBOX_APP_ID")
+	dropboxSecret := os.Getenv("DROPBOX_APP_SECRET")
+
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     dropboxID,
+		ClientSecret: dropboxSecret,
+		Scopes:       []string{"files.metadata.read", "files.content.read"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://www.dropbox.com/oauth2/authorize",
+			TokenURL: "https://api.dropboxapi.com/oauth2/token",
+		},
+	}
+
+	// use PKCE to protect against CSRF attacks
+	// https://www.ietf.org/archive/id/draft-ietf-oauth-security-topics-22.html#name-countermeasures-6
+	verifier := oauth2.GenerateVerifier()
+
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
+	fmt.Printf("Visit the URL for the auth dialog: %v\n", url)
+
+	fmt.Printf("Once you have a code, paste it and press enter: ")
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+	tok, err := conf.Exchange(ctx, code, oauth2.VerifierOption(verifier))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := conf.Client(ctx, tok)
+	resp, err := client.Post(
+		"https://api.dropboxapi.com/2/files/list_folder",
+		"application/json",
+		strings.NewReader(`{
+			"path": ""
+		}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
+}
--- go.mod
+++ go.mod
@@ -4,28 +4,26 @@ go 1.24.1
 
 require (
 	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-mail/mail/v2 v2.3.0
 	github.com/gorilla/csrf v1.7.3
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgx/v5 v5.7.5
+	github.com/joho/godotenv v1.5.1
 	github.com/pressly/goose/v3 v3.25.0
 	golang.org/x/crypto v0.41.0
+	golang.org/x/oauth2 v0.32.0
 )
 
 require (
-	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.3 // indirect
-	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 )
